0ec4a170ce1087b4a2ec1f3a85c1e84ab07501e5
braulioportela79 <braulioportela@outlook.com>
2022-04-21 15:33:45 -0300

    Views & Partials

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,18 +1,20 @@
 import { Request, Response } from 'express';
 
 export const home = (req: Request, res: Response) => {
-    res.send('home controller');
-    // res.render('pages/page');
+    res.render('pages/page');
 };
 
 export const dogs = (req: Request, res: Response) => {
+    res.send('dogs');
     // res.render('pages/page');
 };
 
 export const cats = (req: Request, res: Response) => {
+    res.send('cats');
     // res.render('pages/page');
 };
 
 export const fish = (req: Request, res: Response) => {
+    res.send('fish');
     // res.render('pages/page');
 };
