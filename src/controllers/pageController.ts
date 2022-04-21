a5e879cf6ea488fef8a5b38e9458a6e91ab0bfe6
braulioportela79 <braulioportela@outlook.com>
2022-04-21 15:47:08 -0300

    Pets Pages

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,20 +1,37 @@
 import { Request, Response } from 'express';
 
 export const home = (req: Request, res: Response) => {
-    res.render('pages/page');
+  res.render('pages/page', {
+    banner: {
+      title: 'All pets',
+      background: 'allpets.jpg',
+    },
+  });
 };
 
 export const dogs = (req: Request, res: Response) => {
-    res.send('dogs');
-    // res.render('pages/page');
+  res.render('pages/page', {
+    banner: {
+      title: 'Dogs',
+      background: 'banner_dog.jpg',
+    },
+  });
 };
 
 export const cats = (req: Request, res: Response) => {
-    res.send('cats');
-    // res.render('pages/page');
+  res.render('pages/page', {
+    banner: {
+      title: 'Cats',
+      background: 'banner_cat.jpg',
+    },
+  });
 };
 
 export const fish = (req: Request, res: Response) => {
-    res.send('fish');
-    // res.render('pages/page');
+  res.render('pages/page', {
+    banner: {
+      title: 'Fish',
+      background: 'banner_fish.jpg',
+    },
+  });
 };
